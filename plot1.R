bd9636edf861f65cebf0bffc3017c185cbdc3713
John Romania <john.romania@sykes.com>
2015-08-07 22:13:55 -0600

    fix tabs

--- plot1.R
+++ plot1.R
@@ -1,9 +1,9 @@
 plot1<-function(){
-	# preprocess file with grep to only bring in data for days we will use
+    # preprocess file with grep to only bring in data for days we will use
 
-	df<-read.csv(pipe('grep ^[12]/2/2007 household_power_consumption.txt'),sep=';')
+    df<-read.csv(pipe('grep ^[12]/2/2007 household_power_consumption.txt'),sep=';')
     # set column names
-	colnames(df)<-c('Date','Time','Global_active_power','Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3')
+    colnames(df)<-c('Date','Time','Global_active_power','Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3')
     #combine date and time columns into a datetime column
     df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
     #create png device
