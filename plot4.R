20fbbd2c487344063a8ee4b6a170b799292c6a25
John Romania <john.romania@sykes.com>
2015-08-07 22:05:45 -0600

    plot4 code and png comments

--- plot4.R
+++ plot4.R
@@ -10,15 +10,19 @@ plot4<-function(){
     png('~/datascience/ExData_Plotting1/plot4.png',width=480,height=480)
     par(mfrow=c(2,2))
     
-    #draw plot
+    #draw 1st plot
     plot(df$DateTime,df$Global_active_power,type='l',ylab='Global Active Power',xlab='')
- 	plot(df$DateTime,df$Voltage,type='l',ylab='Voltage',xlab='datetime')
+ 	#draw 2nd plot
+    
+    plot(df$DateTime,df$Voltage,type='l',ylab='Voltage',xlab='datetime')
+    #draw 3rd
     plot(df$DateTime,df$Sub_metering_1,type='l',ylab='Energy sub metering',xlab='')
 	#add additional lines
 	lines(df$DateTime,df$Sub_metering_2,col='red')
 	lines(df$DateTime,df$Sub_metering_3,col='blue')
 	#setup the legend
 	legend("topright",40,c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),lwd=c(1,1),col=c("black","red","blue"),cex=.7,lty=1,bty='n')
+	#4th and final
 	plot(df$DateTime,df$Global_reactive_power,type='l',ylab='Global_reactive_power',xlab='datetime')
     dev.off()
 }
